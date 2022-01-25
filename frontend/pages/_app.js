020c76c1f2fdb2f642a41f73d1eddbf0a291a70c
Charles Andres <charlesandres@charless-mini.lan>
2022-01-24 23:54:22 -0800

    syntax error in app fix

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from "styled-components";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
