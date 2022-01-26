dc071d667c0ecf434f9c563354b8ec2d0d87f26b
Charles Andres <charlesandres@charless-mini.lan>
2022-01-25 22:11:52 -0800

    add first nav menu animation

--- frontend/components/Hamburger.js
+++ frontend/components/Hamburger.js
@@ -1,8 +1,19 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
 const HamburgerContainer = styled.svg`
   height: 21px;
   width: 32px;
+`;
+
+const StyledButton = styled.button`
+  top: 12px;
+  position: fixed;
+  z-index: 10001;
+  right: 16px;
+  width: 32px;
+  height: 32px;
+  @media screen and (min-width: 769px) {
+    display: none;
+  }
 `;
 
 const Line = (props) => <line fill="transparent" strokeWidth="3" {...props} />;
@@ -16,12 +27,14 @@ const middleLine = (
 const bottomLine = (
   <Line x1="0" y1="19" x2="32" y2="19" stroke="hsl(181, 56%, 17%)" />
 );
-export default function Hamburger() {
+export default function Hamburger({ toggle }) {
   return (
-    <HamburgerContainer xmlns="http://www.w3.org/2000/svg">
-      {topLine}
-      {middleLine}
-      {bottomLine}
-    </HamburgerContainer>
+    <StyledButton aria-label="Mobile Navigation Button" onClick={toggle}>
+      <HamburgerContainer xmlns="http://www.w3.org/2000/svg">
+        {topLine}
+        {middleLine}
+        {bottomLine}
+      </HamburgerContainer>
+    </StyledButton>
   );
 }
