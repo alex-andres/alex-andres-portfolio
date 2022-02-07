12d16f44a3a02706512dcbdc176fa9d0801d1378
Charles Andres <charlesalexanderandres@gmail.com>
2022-02-07 12:23:50 -0800

    add spotify currently listening widget ssr

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -33,6 +33,7 @@ const GlobalStyle = createGlobalStyle`
     --color-shadows: 0deg 0% 63%;
     --color-mobile-bg: hsla(0deg, 0%, 100%, 0.75);
     --color-twitter-hover: hsl(180deg 14% 97%);
+    --color-spotify-background: hsl(0, 0%, 95%);
     --shadow-elevation-low:
       0.3px 0.5px 0.7px hsl(var(--color-shadows) / 0.34),
       0.4px 0.8px 1px -1.2px hsl(var(--color-shadows) / 0.34),
@@ -123,6 +124,7 @@ p, h1, h2, h3, h4, h5, h6 {
   --color-shadows: hsl(0, 0% 37%);
   --color-mobile-bg: hsla(0, 0%, 5%, 0.85);
   --color-twitter-hover: hsl(180, 14% 3%);
+  --color-spotify-background: hsl(0, 0%, 9%);
 }
 #__next{
   position: relative;
